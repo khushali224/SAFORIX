125ea6d8bb1b82bc6ea6ca6b6e273f0c2db6ff61
khushali224 <185090805+khushali224@users.noreply.github.com>
2026-04-15 13:59:58 +0530

    Add files via upload

---
+++ Views/Account/LoginRegistration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SAFORIX.Views.Account
+{
+    public class LoginRegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
